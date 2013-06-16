53ebb3f1f423af206e24dfdd13593418824bbf9e
Danny van Heumen <danny@dannyvanheumen.nl>
2013-06-16 23:18:33 +0200

    Working on coordination between routines, output parsing and state discovery.

--- checkdisk.go
+++ checkdisk.go
@@ -3,39 +3,101 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	var cmd = exec.Command("/usr/sbin/badblocks", "-sv", "/dev/sda")
+	var cmd = exec.Command("/usr/sbin/badblocks", "-sv", "testdisk")
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		fmt.Printf("Error: %s", err.Error())
+		log.Printf("Error: %s\n", err.Error())
 		return
 	}
 	//Start process
 	if err = cmd.Start(); err != nil {
-		fmt.Printf("Error starting process: %s", err.Error())
+		log.Printf("Error starting process: %s\n", err.Error())
 		return
 	}
 	//Prepare signal handler
 	stopSignal := make(chan struct{})
 	go signalHandler(stopSignal, cmd.Process)
 	//Start separate reader routine
-	go processHandler(stderr)
+	report := make(chan *CheckState)
+	go processHandler(stderr, report)
 	//Waiting until the process finishes
 	cmd.Wait()
 	close(stopSignal)
+	//Process reported state
+	var state = <-report
+	if block := state.InterruptBlock; block != nil {
+		log.Printf("Check has been interrupted at block %d\n", *block)
+	} else {
+		log.Printf("Successful execution.\n")
+	}
 }
 
-func processHandler(from io.ReadCloser) {
+func signalHandler(stopsig <-chan struct{}, proc *os.Process) {
+	// Register for Interrupt signals.
+	var sigchan = make(chan os.Signal)
+	signal.Notify(sigchan, os.Interrupt)
+	defer signal.Stop(sigchan)
+	// Start signaling loop.
+	var done = false
+	for !done {
+		select {
+		case sig := <-sigchan:
+			proc.Signal(sig)
+		case <-stopsig:
+			done = true
+		}
+	}
+}
+
+var startPattern = regexp.MustCompile(`^Checking blocks (\d+) to (\d+)$`)
+var progressPattern = regexp.MustCompile(`^Checking for bad blocks[^:]*: `)
+var progressDonePattern = regexp.MustCompile(`Checking for bad blocks[^:]*: done`)
+var finishedPattern = regexp.MustCompile(`^Pass completed, (\d+) bad blocks found. \((\d+)/(\d+)/(\d+) errors\)$`)
+var interruptedPattern = regexp.MustCompile(`^Interrupted at block (\d+)$`)
+
+func processHandler(from io.ReadCloser, report chan<- *CheckState) {
 	var out = make(chan []byte)
 	go readLines(from, out)
+	var state *CheckState
 	for l := range out {
-		fmt.Printf("Line: %s\n", l)
+		if startPattern.Match(l) {
+			var matches = startPattern.FindSubmatch(l)
+			from, _ := strconv.ParseUint(string(matches[1]), 10, 64)
+			to, _ := strconv.ParseUint(string(matches[2]), 10, 64)
+			state = &CheckState{From: from, To: to, InterruptBlock: nil}
+		} else if progressPattern.Match(l) {
+			fmt.Println("Progress on disk checking.")
+		} else if progressDonePattern.Match(l) {
+			fmt.Println("Finalized disk checking.")
+		} else if finishedPattern.Match(l) {
+			fmt.Println("Check done.")
+		} else if interruptedPattern.Match(l) {
+			var matches = interruptedPattern.FindSubmatch(l)
+			stopBlock, _ := strconv.ParseUint(string(matches[1]), 10, 64)
+			state.InterruptBlock = &stopBlock
+			fmt.Printf("Interrupted at %d\n", stopBlock)
+		} else if len(l) == 0 {
+			// Nothing to do
+		} else {
+			log.Printf("Ignoring unknown line: %s\n", l)
+		}
 	}
+	report <- state
+}
+
+type CheckState struct {
+	From           uint64
+	To             uint64
+	InterruptBlock *uint64
 }
 
 func readLines(from io.Reader, out chan<- []byte) {
@@ -81,21 +143,4 @@ func send(to chan<- []byte, data []byte) {
 	var result = make([]byte, len(data))
 	copy(result, data)
 	to <- result
-}
-
-func signalHandler(stopsig <-chan struct{}, proc *os.Process) {
-	// Register for Interrupt signals.
-	var sigchan = make(chan os.Signal)
-	signal.Notify(sigchan, os.Interrupt)
-	defer signal.Stop(sigchan)
-	// Start signaling loop.
-	var done = false
-	for !done {
-		select {
-		case sig := <-sigchan:
-			proc.Signal(sig)
-		case <-stopsig:
-			done = true
-		}
-	}
 }
